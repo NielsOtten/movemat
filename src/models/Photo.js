5c928f8964b0a9b690ec778b16dbb1990496f18d
Niels <niels@burst-digital.com>
2017-05-26 08:40:40 +0200

    Photo model

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -1,6 +1,34 @@
 import mongoose from 'mongoose';
 import errorMessages from '../constants/errorMessages';
 
-const Schema = new mongoose.Schema({});
+const Schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: errorMessages.REQUIRED_ERROR,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  group: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Group',
+  },
+  fileType: {
+    type: String,
+  },
+  absolutePath: {
+    type: String,
+    required: errorMessages.REQUIRED_ERROR,
+  },
+  path: {
+    type: String,
+    required: errorMessages.REQUIRED_ERROR,
+  },
+  timestamp: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
 
 export default mongoose.model('Photo', Schema);
