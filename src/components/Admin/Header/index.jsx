7f60f1699a415c257a8791d1a6145b1c1320f400
Niels <niels@burst-digital.com>
2017-07-05 11:41:49 +0200

    Add drawer to header

--- src/components/Admin/Header/index.jsx
+++ src/components/Admin/Header/index.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { observer } from 'mobx-react';
+import { observable } from 'mobx';
 import Appbar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-const Header = () => (
-  <Appbar
-    title='Admin Steps'
-    iconClassNameRight='muidocs-icon-navigation-expand-more'
-  />
-);
+injectTapEventPlugin();
+
+@observer
+class Header extends Component {
+  @observable drawerOpen = false;
+
+  drawerToggle = () => {
+    this.drawerOpen = !this.drawerOpen;
+  };
+
+  drawerClose = () => {
+    this.drawerOpen = false;
+  }
+
+  render() {
+    return (
+      <div>
+        <Appbar
+          title='Admin Steps'
+          iconClassNameRight='muidocs-icon-navigation-expand-more'
+          onLeftIconButtonTouchTap={this.drawerToggle}
+        />
+        <Drawer
+          open={this.drawerOpen}
+          docked={false}
+          onRequestChange={open => this.drawerOpen = open}
+        >
+          <MenuItem onTouchTap={this.drawerClose}>Gebruikers</MenuItem>
+          <MenuItem onTouchTap={this.drawerClose}>Familie</MenuItem>
+        </Drawer>
+      </div>
+    );
+  }
+}
 
 export default Header;
