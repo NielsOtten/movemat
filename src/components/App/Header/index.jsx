4f77a21e70d16e10b197f50c8c8511f8ef09a6a8
Niels <niels@burst-digital.com>
2017-11-06 18:58:14 +0100

    Add link to logo

--- src/components/App/Header/index.jsx
+++ src/components/App/Header/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import { AppBar, NavDrawer, Navigation } from 'react-toolbox';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import Button from '../../common/LinkButtons';
 import AuthStore from '../../../stores/AuthStore';
 import style from './style.scss';
+import { constants as LinkRoutes } from '../index';
 
 @observer
 class Header extends Component {
@@ -13,11 +14,14 @@ class Header extends Component {
   };
 
   render() {
+    const logoRoute = AuthStore.loggedIn ? LinkRoutes.DASHBOARD_ROUTE : LinkRoutes.HOME_ROUTE;
     return (
       <div>
         {this.state.logout && <Redirect to='/' />}
         <AppBar>
-          <img className={style.logo} src='/steps-logo.png' alt='Steps Logo' />
+          <Link to={logoRoute} >
+            <img className={style.logo} src='/steps-logo.png' alt='Steps Logo' />
+          </Link>
           <Navigation type='horizontal' className={style.navigation}>
             {!AuthStore.loggedIn && <Button href='/login' label='Login' className={style.raisedButton} raised /> }
             {!AuthStore.loggedIn && <Button href='/registreer' label='Registreren' className={style.flatButton} flat />}
